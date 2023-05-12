49107feb460beca9edce2e8c5740d63ea8789463
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-05-12 15:05:19 +0530

    Update Checkout.js

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -9,6 +9,7 @@ import NavBar from './NavBar';
 import CustomerDetailsForm from './CustomerDetailsForm';
 import PaymentForm from './PaymentForm';
 import Footer from './Footer';
+import { Link  } from 'react-router-dom';
 
 class Checkout extends Component {
 
