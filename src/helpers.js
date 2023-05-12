bee974f540549ce958fa38097cff9a8853729e08
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-05-12 15:21:03 +0530

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 export function formatPrice(cents) {
-  return (cents / 100).toLocaleString("en-UK", {
+  return (cents).toLocaleString("en-UK", {
     style: "currency",
-    currency: "GBP"
+    currency: "INR"
   });
 }
 
