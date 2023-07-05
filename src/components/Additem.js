0699cbf8b39e09a94b53af2862a192b432b7b45a
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-07-05 17:16:17 +0530

    Update Additem.js

--- src/components/Additem.js
+++ src/components/Additem.js
@@ -3,20 +3,19 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Header, Container } from 'semantic-ui-react';
 
-import NavBar from './NavBar';
 import Footer from './Footer';
-import PizzaContainer from './PizzaContainer';
+import SplContainer from './SplContainer';
 
 const Additem = (props) => (
   <Fragment>
-    <NavBar order={props.order} orderTotal={props.orderTotal}>
+    
       
-    </NavBar>
+    
     <Container id='page-container'>
       <Header as='h1' id='page-header'>Today's Special items</Header>
      
       <Container id='menu-pizza-container'>
-          <PizzaContainer addToOrder={props.addToOrder} />
+          <SplContainer addToOrder={props.addToOrder} />
       </Container>
     </Container>
     <Footer />
