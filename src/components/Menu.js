f6835e1e51225fd7c99f44161fe9d0702f214669
RajiveeJ <99588388+RajiveeJ@users.noreply.github.com>
2023-07-06 16:26:58 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,40 +1,28 @@
-import React, {Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Link  } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Header, Container, Button } from 'semantic-ui-react';
 
 import NavBar from './NavBar';
 import Footer from './Footer';
-import Item from './Item';
+
 import PizzaContainer from './PizzaContainer';
 
 
-class Menu extends Component {
-
-  static propTypes = {
-   order: PropTypes.array,
-  orderTotal: PropTypes.number,
-  addToOrder: PropTypes.func.isRequired,
-  addToItem: PropTypes.func.isRequired,
-  }
-  render(){
-    return(
-      <Fragment>
-         <NavBar order={props.order} orderTotal={props.orderTotal}>
-            {props.orderTotal > 0 && <Button as={Link} to='/cart' color='teal' id='menu-checkout-btn'>Checkout</Button>}
-         </NavBar>
-         <Container id='page-container'>
-         <Header as='h1' id='page-header'>Pizza Selection</Header>
-         <Container textAlign='center' id='menu-free-banner'>
-         <p id='menu-banner-text'>Free delivery for orders over Rs.1000</p>
-         </Container>
-         <Container id='menu-pizza-container'>
+const Menu = (props) => (
+   
+  <Fragment>
+    <NavBar order={props.order} orderTotal={props.orderTotal}>
+      {props.orderTotal > 0 && <Button as={Link} to='/cart' color='teal' id='menu-checkout-btn'>Checkout</Button>}
+    </NavBar>
+    <Container id='page-container'>
+      <Header as='h1' id='page-header'>Pizza Selection</Header>
+      <Container textAlign='center' id='menu-free-banner'>
+        <p id='menu-banner-text'>Free delivery for orders over Rs.1000</p>
+      </Container>
+      <Container id='menu-pizza-container'>
           <PizzaContainer addToOrder={props.addToOrder} />
-          <Item
-             
-              addToItem={this.props.addToItem}
-              
-            />
+          
       </Container>
      
       
@@ -42,7 +30,12 @@ class Menu extends Component {
     <Footer />
   </Fragment>
 );
-}
-}
+
+Menu.propTypes = {
+  order: PropTypes.array,
+  orderTotal: PropTypes.number,
+  addToOrder: PropTypes.func.isRequired,
+  addToItem: PropTypes.func.isRequired
+};
 
 export default Menu;
